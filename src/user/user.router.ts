55df575fa78a35060c8b2c47671c232f6fe6617d
Hamid Berkaine <hamiddev78@gmail.com>
2023-02-10 11:54:29 +0100

    add router for fetch a user

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -15,3 +15,17 @@ userRouter.get("/", async (req: Request, res: Response) => {
         return res.status(500).json(err.message)
     }
 })
+
+// GET: A single user by Id
+userRouter.get("/:id", async (req, res) => {
+    const id: string = req.params.id
+    try {
+        const user = await UserService.getUser(id)
+        if (user) {
+            return res.status(200).json(user)
+        }
+        return res.status(404).json("User not found")
+    } catch (err: any) {
+        return res.status(500).json(err.message)
+    }
+})
