64ece8feaa9c85b111804f7a484f0221352b781f
Hamid Berkaine <hamiddev78@gmail.com>
2023-02-11 10:43:19 +0100

    add router to fetch the list of the plants

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 
 import { userRouter } from "./user/user.router";
+import { plantRouter } from "./plant/plant.router";
 
 dotenv.config();
 // if not port don't just exiting application
@@ -17,6 +18,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRouter)
+app.use("/api/plants", plantRouter)
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
