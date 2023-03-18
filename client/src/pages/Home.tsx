70d90e51c2055422f11905d37578d0e73a9af30f
Hamid Berkaine <hamiddev78@gmail.com>
2023-03-18 15:39:52 +0100

    add function to fetch data inside the database

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,58 @@
+import React, { Fragment, useState, useEffect } from "react";
 
+import "../css/home.css";
 
 function Home() {
+    useEffect(() => {
+        const getAPI = () => {
+
+            const API_LOCAL: string = 'http://localhost:8000/api/plants/';
+
+
+            fetch(API_LOCAL)
+                .then((res) => {
+                    console.log('result: ', res);
+                    return res.json();
+                })
+                .then((data) => {
+                    console.log('data:', data);
+                    setLoading(false);
+                    setApiData(data);
+                });
+        };
+        getAPI();
+    }, []);
+
+    const [apiData, setApiData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    { console.log('api data: ', apiData) }
     return (
-        <main>Home</main>
+        <Fragment>
+            <header>
+                <h1>Welcome to the home page</h1>
+            </header>
+            <main>
+
+                {loading === true ? (
+                    <h2>Loading...</h2>
+                ) : (
+                    <section>
+                        {
+                            // apiData.map((items, key) => {
+                            //     return (
+                            //         <section key={key}>
+                            //             <article>
+                            //                 <h2>{items}</h2>
+                            //             </article>
+                            //         </section>
+                            //     )
+                            // })
+                        }
+                    </section>
+                )
+                }
+            </main>
+        </Fragment >
     )
 }
 
