e2b0f70be362ecec22cddc451283c9a39f4be157
Hamid Berkaine <hamiddev78@gmail.com>
2023-03-27 15:48:37 +0200

    redirect file and add route

--- client/src/components/routes/routes.tsx
+++ client/src/components/routes/routes.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Home from '../../pages/Home';
-import Plants from '../../pages/Plants';
-import Articles from '../../pages/Articles';
-import Blog from '../../pages/Blog';
+import Home from '../../pages/home/Home';
+import Plants from '../../pages/Plants/Plants';
+import Articles from '../../pages/articles/Articles';
+import Blog from '../../pages/blog/Blog';
+import Login from '../../pages/login/Login';
 
 const RoutesApp = () => {
     return (
@@ -12,6 +13,7 @@ const RoutesApp = () => {
             <Route path='/plants' element={<Plants />} />
             <Route path='/articles' element={<Articles />} />
             <Route path='/blog' element={<Blog />} />
+            <Route path='/login' element={<Login />} />
         </Routes>
     )
 }
