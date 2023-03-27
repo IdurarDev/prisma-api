40037a45f17df88ac97eb06ca7727342914f8e15
Hamid Berkaine <hamiddev78@gmail.com>
2023-03-27 16:39:37 +0200

    add form and validation schema with yup

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import * as Yup from "yup"
+import { Form, Formik } from "formik"
 
 function Login() {
-    return (
-        <div>Login</div>
+    const schema = Yup.object({
+        Email: Yup.string().email().required(),
+        Password: Yup.string().matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, "The password is required: a uppercase letter and a number min and a special character").required()
+    })
+
+    return (<section className='main-container-form'>
+        <div>
+            <h2>Log in</h2>
+            <Formik
+                initialValues={{ Email: "", Password: "" }}
+                onSubmit={value => console.log(value)}
+                validationSchema={schema}
+            >
+                {
+                    <Form>
+                        <div>
+                            <input type="email" name="email" />
+                        </div>
+                        <div>
+                            <input type="password" name="password" />
+                        </div>
+                        <div>
+                            <button>Submit</button>
+                        </div>
+                    </Form>
+                }
+            </Formik>
+        </div>
+    </section>
     )
 }
 
