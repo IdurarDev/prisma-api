16eabb7a3b95403d86f38bf2d41625773c07b729
Hamid Berkaine <hamiddev78@gmail.com>
2023-03-30 12:32:34 +0200

    add function to fetch the list of articles

---
+++ src/article/article.service.ts
@@ -0,0 +1,31 @@
+import { db } from '../utils/db.server';
+import type { User } from '../user/user.service';
+
+type Article = {
+    id: string;
+    title: string;
+    description: string;
+    datePublished: Date;
+    user: User;
+    // userId: string;
+}
+
+export const ListArticles = async (): Promise<Article[]> => {
+    return db.article.findMany({
+        select: {
+            id: true,
+            title: true,
+            description: true,
+            datePublished: true,
+            user: {
+                select: {
+                    id: true,
+                    firstname: true,
+                    lastname: true,
+                    email: true,
+                }
+            }
+            // userId: true,
+        }
+    })
+}
