7a0916fdf6d2aa73a93380ece789fa03921dead5
Hamid Berkaine <hamiddev78@gmail.com>
2023-04-01 12:06:30 +0200

    add function to fetch all articles from database and display it to the article page

--- client/src/pages/articles/Articles.tsx
+++ client/src/pages/articles/Articles.tsx
@@ -1,8 +1,56 @@
-import React from 'react'
-
+import { Fragment, useState, useEffect } from 'react'
+import { Article } from '@prisma/client';
+import bannerArticle from '../../images/article-banner.jpg';
 function Articles() {
+    useEffect(() => {
+        getArticlesApi();
+    }, []);
+
+    const getArticlesApi = (): void => {
+        const ARTICLES_API: string = 'http://localhost:8000/api/articles/';
+
+        fetch(ARTICLES_API)
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                setLoading(false);
+                setArticleApi(data);
+            })
+    };
+
+    const [loading, setLoading] = useState(true);
+    const [articleApi, setArticleApi] = useState<Article[]>([])
     return (
-        <div>Articles</div>
+        <Fragment>
+            <main>
+                <img className='bannerArticle' src={bannerArticle} alt="une belle bouteille au milieu de feuilles vertes" />
+                <section className='text-over-banner'>
+                    <article>
+                        <h2>Articles sur les plantes</h2>
+                        <p>Bienfaits et vertus des plantes et santé, propriétés médicinales, utilisation en cuisine et thérapeutique pour soigner et prévenir les maladies</p>
+                    </article>
+                </section>
+
+                <h3>Les articles récents</h3>
+                {loading === true ? (
+                    <h2>Loading...</h2>
+                ) : (
+                    <section className='all-articles'>
+                        {
+                            articleApi.map((art, key) => {
+                                return (
+                                    <article key={key}>
+                                        <h3>{art.title}</h3>
+                                        <p>{art.description}</p>
+                                    </article>
+                                )
+                            })
+                        }
+                    </section>
+                )}
+            </main>
+        </Fragment>
     )
 }
 
