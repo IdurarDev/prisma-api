8a628ece38453ee68ffcb2f7917a677c91bb90e3
Hamid Berkaine <hamiddev78@gmail.com>
2023-05-02 16:21:33 +0200

    add function to fetch an blog

--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -27,3 +27,23 @@ export const listBlogs = async (): Promise<Blog[]> => {
         }
     })
 }
+
+export const getBlog = async (id: string): Promise<Blog | null> => {
+    return db.blog.findUnique({
+        where: { id, },
+        select: {
+            id: true,
+            title: true,
+            description: true,
+            datePublished: true,
+            user: {
+                select: {
+                    id: true,
+                    firstname: true,
+                    lastname: true,
+                    email: true,
+                }
+            }
+        }
+    })
+}
