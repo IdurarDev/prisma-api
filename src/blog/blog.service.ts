63ec4824d012b8b46b52f10553cf5839e23aa827
Hamid Berkaine <hamiddev78@gmail.com>
2023-05-02 16:38:16 +0200

    add function to create a new blog

--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -9,6 +9,13 @@ type Blog = {
     user: User;
 }
 
+type NewBlog = {
+    title: string;
+    datePublished: Date;
+    description: string;
+    userId: string;
+}
+
 export const listBlogs = async (): Promise<Blog[]> => {
     return db.blog.findMany({
         select: {
@@ -31,6 +38,34 @@ export const listBlogs = async (): Promise<Blog[]> => {
 export const getBlog = async (id: string): Promise<Blog | null> => {
     return db.blog.findUnique({
         where: { id, },
+        select: {
+            id: true,
+            title: true,
+            description: true,
+            datePublished: true,
+            user: {
+                select: {
+                    id: true,
+                    firstname: true,
+                    lastname: true,
+                    email: true,
+                }
+            }
+        }
+    })
+}
+
+export const createBlog = async (blog: NewBlog): Promise<Blog> => {
+    const { title, userId, description, datePublished } = blog;
+    const parsedDate: Date = new Date(datePublished);
+
+    return db.blog.create({
+        data: {
+            title,
+            description,
+            userId,
+            datePublished: parsedDate,
+        },
         select: {
             id: true,
             title: true,
