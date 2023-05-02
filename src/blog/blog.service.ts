51589fdfa18cd2b9e926637bf64d96501e3b7024
Hamid Berkaine <hamiddev78@gmail.com>
2023-05-02 16:07:02 +0200

    add function to fetch all blog

---
+++ src/blog/blog.service.ts
@@ -0,0 +1,29 @@
+import { db } from '../utils/db.server';
+import type { User } from '../user/user.service';
+
+type Blog = {
+    id: string;
+    title: string;
+    description: string;
+    datePublished: Date;
+    user: User;
+}
+
+export const listBlogs = async (): Promise<Blog[]> => {
+    return db.blog.findMany({
+        select: {
+            id: true,
+            title: true,
+            description: true,
+            datePublished: true,
+            user: {
+                select: {
+                    id: true,
+                    firstname: true,
+                    lastname: true,
+                    email: true,
+                }
+            }
+        }
+    })
+}
