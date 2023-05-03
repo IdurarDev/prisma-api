07e854216df83e4943deb49fbb6d06f97ee51446
Hamid Berkaine <hamiddev78@gmail.com>
2023-05-03 13:14:32 +0200

    update function to fetch all data and display in the home page

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,51 +1,60 @@
 import { Fragment, useState, useEffect } from "react";
 import { Plant } from "@prisma/client";
+import { Blog } from '@prisma/client';
+import { Article } from '@prisma/client';
+
+import { fetchApi } from "../../components/fetchApi";
+
 import bannerPlant from "../../images/plant-banner.jpg"
 import "../../css/home.css";
 
 function Home() {
-    useEffect(() => {
-        getAPI();
-    }, []);
-
-    const getAPI = (): void => {
-        const API_LOCAL: string = 'http://localhost:8000/api/plants/';
-
-        fetch(API_LOCAL)
-            .then((res) => {
-                return res.json();
-            })
-            .then((data) => {
-                setLoading(false);
-                setApiData(data);
-            });
-    };
-
     const [apiData, setApiData] = useState<Plant[]>([]);
-    const [loading, setLoading] = useState(true);
+    const [articleApi, setArticleApi] = useState<Article[]>([])
+    const [blogApi, setBlogApi] = useState<Blog[]>([])
+
+    useEffect(() => {
+        fetchApi('http://localhost:8000/api/plants/', setApiData)
+        fetchApi('http://localhost:8000/api/articles/', setArticleApi)
+        fetchApi('http://localhost:8000/api/blog/', setBlogApi)
+    }, []);
 
     return (
         <Fragment>
             <main>
                 <img className="bannerHome" src={bannerPlant} alt="une jolie bannière de plantes" />
-
-                {loading === true ? (
-                    <h2>Loading...</h2>
-                ) : (
-                    <section className="main-section">
-                        {
-                            apiData.map((plant, key) => {
-                                return (
-                                    <article className="home-img" key={key}>
-                                        <h3>{plant.title}</h3>
-                                        <p>{plant.description}</p>
-                                    </article>
-                                )
-                            })
-                        }
-                    </section>
-                )
-                }
+                <section className="main-section">
+                    {
+                        apiData.map((plant, key) => {
+                            return (
+                                <article className="home-img" key={key}>
+                                    <h3>{plant.title}</h3>
+                                    <p>{plant.description}</p>
+                                </article>
+                            )
+                        })
+                    }
+                    {
+                        articleApi.map((art, key) => {
+                            return (
+                                <article className='one-article' key={key}>
+                                    <h3>{art.title}</h3>
+                                    <p>{art.description}</p>
+                                </article>
+                            )
+                        })
+                    }
+                    {
+                        blogApi.map((blog, key) => {
+                            return (
+                                <article className="one-article" key={key}>
+                                    <h3>{blog.title}</h3>
+                                    <p>{blog.description}</p>
+                                </article>
+                            )
+                        })
+                    }
+                </section>
             </main>
         </Fragment >
     )
