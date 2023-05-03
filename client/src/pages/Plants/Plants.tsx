e5fedc23d245bba23078b3290db189ff23de58cd
Hamid Berkaine <hamiddev78@gmail.com>
2023-05-03 12:20:35 +0200

    add function to fetch all plant and display in the plant page

--- client/src/pages/Plants/Plants.tsx
+++ client/src/pages/Plants/Plants.tsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import { Fragment, useState, useEffect } from "react";
+import { Plant } from "@prisma/client";
+
+import bannerPlant from "../../images/plant-banner.jpg"
+import "../../css/home.css";
 
 function Plants() {
+    useEffect(() => {
+        getAPI();
+    }, []);
+
+    const getAPI = (): void => {
+        const API_LOCAL: string = 'http://localhost:8000/api/plants/';
+
+        fetch(API_LOCAL)
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                setLoading(false);
+                setApiData(data);
+            });
+    };
+
+    const [apiData, setApiData] = useState<Plant[]>([]);
+    const [loading, setLoading] = useState(true);
+
     return (
-        <div>Plants</div>
+        <Fragment>
+            <main>
+                <img className="bannerHome" src={bannerPlant} alt="une jolie bannière de plantes" />
+
+                {loading === true ? (
+                    <h2>Loading...</h2>
+                ) : (
+                    <section className="main-section">
+                        {
+                            apiData.map((plant, key) => {
+                                return (
+                                    <article className="home-img" key={key}>
+                                        <h3>{plant.title}</h3>
+                                        <p>{plant.description}</p>
+                                    </article>
+                                )
+                            })
+                        }
+                    </section>
+                )
+                }
+            </main>
+        </Fragment >
     )
 }
 
