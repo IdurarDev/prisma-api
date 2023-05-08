7270ba8df0a292eac9f1b163784dddf59a85ef11
Hamid Berkaine <hamiddev78@gmail.com>
2023-05-08 13:02:44 +0200

    create subscribe page

---
+++ client/src/pages/subscribe/Subscribe.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+function Subscribe() {
+    return (
+        <Fragment>
+            <h3>Welcome for you</h3>
+            <div>subscribe here !</div>
+        </Fragment>
+    )
+}
+
+export default Subscribe
