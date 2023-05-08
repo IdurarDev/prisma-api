183371c20c805e2fe73002feea27dd6944285d4d
Hamid Berkaine <hamiddev78@gmail.com>
2023-05-08 13:12:27 +0200

    add link to return in the login page

--- client/src/pages/subscribe/Subscribe.tsx
+++ client/src/pages/subscribe/Subscribe.tsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 function Subscribe() {
     return (
         <Fragment>
             <h3>Welcome for you</h3>
             <div>subscribe here !</div>
+            <Link to="/login">Return to login page</Link>
         </Fragment>
     )
 }
