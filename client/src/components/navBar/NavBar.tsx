683457996fa61b64e3eba12e6329be91d9e76890
Hamid Berkaine <hamiddev78@gmail.com>
2023-05-09 12:41:57 +0200

    change class name and modif component in the navbar

--- client/src/components/navBar/NavBar.tsx
+++ client/src/components/navBar/NavBar.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo_site from '../../images/logo_site.png';
+import SearchBar from '../searchBar/SearchBar';
 
 import { IoIosPerson } from "react-icons/io";
-import { IoMdSearch } from "react-icons/io";
 
+import logo_site from '../../images/logo_site.png';
 import '../../css/navBar.css';
 
 function NavBar() {
@@ -16,8 +16,8 @@ function NavBar() {
                         <img className='logo' src={logo_site} alt="Le logo du site" />
                     </Link>
                 </figure>
-                <div className='connect'><Link to="/login"><IoIosPerson /></Link></div>
-                <div className='search'><Link to="/"><IoMdSearch /></Link></div>
+                <div className='connect-login'><Link to="/login"><IoIosPerson /></Link></div>
+                <div className='search-bar'><Link to="/"><SearchBar /></Link></div>
             </section>
             <nav className='navbar-header'>
                 <ul>
