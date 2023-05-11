018309b14262308e632c84e53608952010610d1c
Hamid Berkaine <hamiddev78@gmail.com>
2023-05-11 17:49:50 +0200

    update search bar component

--- client/src/components/searchBar/SearchBar.tsx
+++ client/src/components/searchBar/SearchBar.tsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import { useState } from 'react'
-
-import { Plant } from '@prisma/client';
-import { Article } from '@prisma/client';
-import { Blog } from '@prisma/client';
-
+import React, { useState } from 'react'
+import ShowData from './showData';
 
 import { IoMdSearch } from "react-icons/io";
 
 import '../../css/searchBar.css';
 
 const SearchBar = () => {
-    const [buttonSearch, setButtonSearch] = useState(false)
+    const [buttonSearch, setButtonSearch] = useState(false);
+    const [convertTextMin, setConvertTextMin] = useState('');
 
-    const [plant, setPlant] = useState<Plant[]>([])
-    const [article, setArticle] = useState<Article[]>([])
-    const [blog, setBlog] = useState<Blog[]>([])
-
+    let inputHandler = (e: any) => {
+        var lowerCase = e.target.value.toLowerCase();
+        setConvertTextMin(lowerCase);
+    }
 
     return (
         <>
             <div
                 onClick={() => setButtonSearch(!buttonSearch)}
-                className='search'
             >
                 <div className='icon-search'><IoMdSearch /></div>
             </div>
             {buttonSearch && (
                 <div className="searching-bar">
-                    <input className='input-search-bar' />
+                    <input
+                        className='input-search-bar'
+                        placeholder='Search an Plant, Article, Blog...'
+                        onChange={inputHandler}
+                    />
+                    <ShowData data={convertTextMin} />
                 </div>
             )}
         </>
